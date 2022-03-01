15d0779614ccd0428bb47bc308bea8f7f0509ea0
pvaddepa <phalgun.vaddepalli@csgi.com>
2022-03-01 20:33:59 +0530

    Fixed to copy index.html to serve contents of bundle. See https://stackoverflow.com/questions/71301444/cannot-get-webpack-dev-server/71301495#71301495

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');  
 module.exports = {  
     entry: './src/index.ts',  
@@ -8,14 +9,22 @@ module.exports = {
           test: /\.tsx?$/,  
           use: 'ts-loader',  
           exclude: /node_modules/  
-        }  
+        },
+	{
+           test: /\.html$/,
+           loader: 'html-loader'
+        }
       ]  
     },  
     resolve: {  
       extensions: [ '.tsx', '.ts', '.js' ]  
     },  
     output: {  
-      filename: 'bundle.js',  
-      path: path.resolve(__dirname, 'dist')  
-    }  
+      filename: 'index_bundle.js',  
+      path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [new HtmlWebpackPlugin({
+	template: 'src/index.html'
+    })],
+
 };  
